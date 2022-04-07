2753dd3d21311379323933837798cd3ae6395a11
Lapsey <64510365+Lapsey@users.noreply.github.com>
2022-04-06 21:11:53 -0700

    Add files via upload

---
+++ Unit 5/StudentNames/StudentNames/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmStudentNames
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstStuNames = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstStuNames
+        '
+        Me.lstStuNames.FormattingEnabled = True
+        Me.lstStuNames.ItemHeight = 15
+        Me.lstStuNames.Location = New System.Drawing.Point(12, 12)
+        Me.lstStuNames.Name = "lstStuNames"
+        Me.lstStuNames.Size = New System.Drawing.Size(181, 139)
+        Me.lstStuNames.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(254, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(111, 72)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add Names"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(254, 90)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(111, 61)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'frmStudentNames
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(397, 187)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstStuNames)
+        Me.Name = "frmStudentNames"
+        Me.Text = "Student Names"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstStuNames As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSearch As Button
+End Class
---
+++ Unit 5/StudentNames/StudentNames/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class frmStudentNames
+    Const intNumNames As Integer = 5
+    Dim strStuNames(intNumNames - 1)
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        For intI As Integer = 0 To strStuNames.Length - 1
+            strStuNames(intI) = InputBox("Enter a student name")
+        Next
+
+        Dim strReversedArr(strStuNames.Length - 1) As String
+        Dim intFor, intBack
+
+        intBack = strStuNames.Length - 1
+
+        For intFor = 0 To strStuNames.Length - 1
+            strReversedArr(intFor) = strStuNames(intBack)
+            intBack -= 1
+        Next
+
+        strStuNames = strReversedArr
+
+        For intI = 0 To strStuNames.Length - 1
+            lstStuNames.Items.Add(strStuNames(intI))
+        Next
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim strSearchTerm As String = InputBox("Who ya wanna find?")
+
+        For intPos As Integer = 0 To strStuNames.Length - 1
+            If (strStuNames(intPos) = strSearchTerm) Then
+                MessageBox.Show("the term " & strSearchTerm & " is found at position " & intPos)
+                Return
+            End If
+        Next
+
+        MessageBox.Show("the term " & strSearchTerm & " is not in the array")
+    End Sub
+End Class
